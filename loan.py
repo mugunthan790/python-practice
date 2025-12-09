f898adf0dd6b1ecd082ef9c38675a3e8325ea56d
Mugunthan <smugunthan790@gmail.com>
2025-12-09 20:35:30 +0530

    Add loan.py to determine eligibility based on salary and age

---
+++ loan.py
@@ -0,0 +1,6 @@
+salary=int(input("Enter the salary amount:"))
+age=int(input("Enter the age:"))
+if(salary>=20000 or age>=25):
+    print("you are eligible")
+else:
+    print("you are not eligible !")
