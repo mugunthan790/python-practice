70fb0ef1f6baadec27409c007c22849c3e84b5a2
Mugunthan <smugunthan790@gmail.com>
2025-12-09 20:51:07 +0530

    Enhance loan eligibility check with loan amount input and validation

--- loan.py
+++ loan.py
@@ -2,5 +2,10 @@ salary=int(input("Enter the salary amount:"))
 age=int(input("Enter the age:"))
 if(salary>=20000 or age>=25):
     print("you are eligible")
+    lone=int(input("Enter the loan amount :"))
+    if(lone<=50000):
+        print(f"you are eligible for loan {lone}")
+    else:
+        print(" the maximum loan amount is 50,000")
 else:
-    print("you are not eligible !")
+    print("you are not eligible for loan !")
