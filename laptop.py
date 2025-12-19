7cbfc5642959839a54ca682a13fd0a95a346d40e
Mugunthan <smugunthan790@gmail.com>
2025-12-19 19:01:02 +0530

    Add laptop class with attributes and instances for Lenovo, Dell, and Acer

---
+++ laptop.py
@@ -0,0 +1,43 @@
+class laptop:
+    price=""
+    ram=""
+    storage=""
+    pro=""
+    
+lenovo=laptop()
+dell=laptop()
+acer=laptop()
+
+lenovo.price="LENOVO 62000"
+dell.price="DELL 75000"
+acer.price="ACER 50000"
+
+lenovo.pro="i7"
+dell.pro="i5"
+acer.pro="i5"
+
+lenovo.ram="32GB RAM"
+dell.ram="16GB RAM"
+acer.ram="16GB RAM"
+
+lenovo.storage="1TB STORAGE"
+dell.storage="512GB STORAGE"
+acer.storage="512GB STORAGE"
+
+print(lenovo.price)
+print(lenovo.pro)
+print(lenovo.ram)
+print(lenovo.storage)
+print()
+
+print(dell.price)
+print(dell.pro)
+print(dell.ram)
+print(dell.storage)
+print()
+
+print(acer.price)
+print(acer.pro)
+print(acer.ram)
+print(acer.storage)
+        
