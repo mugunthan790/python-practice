8e43208eedbe9c103c4538dc9b0d2ca775f4397e
Mugunthan <smugunthan790@gmail.com>
2025-12-20 18:43:20 +0530

    Add second student instance with display method

--- student.py
+++ student.py
@@ -9,5 +9,11 @@ class student():
 XIth=student()
 XIth.name="mugunthan"
 XIth.reg="24it29"
+print()
 XIth.display()
+print()
+XII=student()
+XII.name="karthi"
+XII.reg="24it27"
+XII.display()
         
