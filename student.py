131db889c166a9f87234d69bcb51689d8b176043
Mugunthan <smugunthan790@gmail.com>
2025-12-20 18:39:33 +0530

    Add student class with initialization and display methods

---
+++ student.py
@@ -0,0 +1,13 @@
+class student():
+    def __init__(self):
+        self.name=""
+        self.reg=""
+    def display(self):
+        print(f"Name:{self.name}")
+        print(f"REG:{self.reg}")
+        
+XIth=student()
+XIth.name="mugunthan"
+XIth.reg="24it29"
+XIth.display()
+        
