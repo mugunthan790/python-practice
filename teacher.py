18498b8935fc698a3a0bd2f8b98a7e2e84813491
Mugunthan <smugunthan790@gmail.com>
2025-12-22 19:24:32 +0530

    Add teacher class with initialization and display methods

---
+++ teacher.py
@@ -0,0 +1,15 @@
+class mam():
+    def __init__(self,nam,num):
+        self.name=""
+        self.reg=""
+    def dis(self):
+        print(f"Name of the teacher is : {self.name}")
+        print(f"Name of the teacher is : {self.reg}")
+        
+staff1 = mam()
+staff1.name="kavitha"
+staff1.reg="24it29"
+staff1.dis()
+
+
+    
