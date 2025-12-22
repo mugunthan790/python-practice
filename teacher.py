bbc950b07ba33d004b928600f42c6840a78b29c6
Mugunthan <smugunthan790@gmail.com>
2025-12-22 19:38:25 +0530

    Fix attribute names in teacher class for consistency and clarity

--- teacher.py
+++ teacher.py
@@ -1,10 +1,10 @@
 class teacher():
     def __init__(self,name,reg):
-        self.name=name
-        self.reg=reg
+        self.tname=name
+        self.regno=reg
     def dis(self):
-        print(f"Name of the teacher is : {self.name}")
-        print(f"Name of the teacher is : {self.reg}")
+        print(f"Name of the teacher is : {self.tname}")
+        print(f"Name of the teacher is : {self.regno}")
 print()       
 t1=teacher("kavitha","24it29")
 t1.dis()
