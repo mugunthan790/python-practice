5fb2cccb1ee817f14464d27265adaf47bc2c0b7c
Mugunthan <smugunthan790@gmail.com>
2025-12-22 20:02:50 +0530

    Add calculator class with basic arithmetic operations and example usage

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class calculator():
+    def __init__(self,num1,num2):
+        self.a=num1
+        self.b=num2
+    def operation(self):
+        oper=input("Enter the operation:")
+        if oper=="add":
+            print("Addition:",self.a+self.b )
+        elif oper=="sub":
+            print("subtraction:",self.a-self.b)
+        elif oper=="mul":
+            print("multiplication:",self.a*self.b)
+        elif oper=="div":
+            print("division:",self.a/self.b)
+        else:
+            print("invalid option !")
+        
+fun=calculator(10,20)
+fun.operation()
+fun2=calculator(2,10)
+fun2.operation()
