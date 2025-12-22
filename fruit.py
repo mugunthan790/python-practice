c82256f332c2d176cb8263e1a7622ddc032ce694
Mugunthan <smugunthan790@gmail.com>
2025-12-22 19:36:21 +0530

    Fix parameter name in fruit class constructor for clarity

--- fruit.py
+++ fruit.py
@@ -1,6 +1,6 @@
 class fruit():
-    def __init__(self,col):
-        self.colour=col
+    def __init__(self,colour):
+        self.colour=colour
     def display(self):
         print(self.colour)
 
