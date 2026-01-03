2a0209de489f2293c4e07b6e262ae4d4fec16708
Mugunthan <smugunthan790@gmail.com>
2026-01-03 19:47:21 +0530

    find even numbers using class objects

---
+++ printEvevNum.py
@@ -0,0 +1,13 @@
+
+
+class eve:
+     def getdata(self):
+        self.num=int(input("enter the number:"))
+        
+     def process(self):
+        for i in range(1,self.num+1):
+            if i %2==0:
+                print(i)
+num1=eve()
+num1.getdata()
+num1.process()
