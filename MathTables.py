5139661483a086e24d246ad48ed928df770b6a43
Mugunthan <smugunthan790@gmail.com>
2026-01-09 20:52:05 +0530

    maths tables

--- MathTables.py
+++ MathTables.py
@@ -1,4 +1,5 @@
-class tables:
+try: 
+ class tables:
     def getdata(self):
         self.n=int(input("Enter the number:"))
         
@@ -6,8 +7,12 @@ class tables:
         for i in range(1,11):
           print(f"{i}*{self.n}={i*self.n}")
           
-execute=tables()
-execute.getdata()
-execute.process()
+ execute=tables()
+ execute.getdata()
+ execute.process()
+except Exception as a:
+    print(f"invalid option {a}")
+finally:
+    print("exit")
         
             
