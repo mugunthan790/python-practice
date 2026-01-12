3f29f12590e25c643d4c7adf7f6458d72598afdc
Mugunthan <smugunthan790@gmail.com>
2026-01-12 19:20:31 +0530

    prime number checker

--- primeChecker.py
+++ primeChecker.py
@@ -1,20 +1,20 @@
 try:
  class prime:
     def getdata(self):
-        self.num=int(input("Enter the number:"))
+        self.n=int(input("Enter the number:"))
         
     def process(self):
-        if self.num==0 or self.num==1:
-            print(f"{self.num} is not a prime number ")
+        if self.n==0 or self.n==1:
+            print(f"{self.n} is not a prime number ")
         
         self.count=0
-        for i in range(1,self.num+1):
-                if(self.num%i==0):
+        for i in range(1,self.n+1):
+                if(self.n%i==0):
                     self.count+=1
         if self.count ==2:
-            print(f"{self.num} is a prime number")
+            print(f"{self.n} is a prime number")
         else:
-            print(f"{self.num} is not a prime number")
+            print(f"{self.n} is not a prime number")
                     
  num1=prime()
  num1.getdata()
